dbd427d86cd1af61ccdea335f2b1ff60f70a1272
Victor Liu <vliu@ibm.com>
2020-01-30 13:13:27 -0600

    Add deck functions

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	// a slice of type string
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
+	hand, remainingCards := deal(cards, 5)
 
-func newCard() string {
-	return "Five of Diamonds"
+	hand.print()
+	remainingCards.print()
 }
---
+++ test.go
@@ -0,0 +1,5 @@
+type laptopSize float64
+
+func (this laptopSize) getSizeOfLaptop() laptopSize {
+	return this
+}
