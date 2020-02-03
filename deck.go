20150269cd302c78134586a006eb9669b6f30b3e
Victor Liu <vliu@ibm.com>
2020-02-03 12:39:04 -0600

    Use time and rand pkg to generate random shuffler func

--- deck.go
+++ deck.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -41,4 +44,30 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+// byte slice and error object
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	// time and rand pkgs
+	source := rand.NewSource(time.Now().UnixNano())
+	// use source to generate new rand num generator, r, to randomize deck
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		// swap
+		d[i], d[newPosition] = d[newPosition], d[i]
+
+	}
 }
--- main.go
+++ main.go
@@ -2,5 +2,6 @@ package main
 
 func main() {
 	cards := newDeck()
-	cards.saveToFile("pacman")
+	cards.shuffle()
+	cards.print()
 }
