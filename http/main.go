c30c4ae73904a344df5c51e318121377b8f5bb3c
Victor Liu <victorliu716@gmail.com>
2020-02-09 22:35:29 -0500

    Use own Write function

--- http/main.go
+++ http/main.go
@@ -7,11 +7,18 @@ import (
 	"io"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	return 1, nil
 }
