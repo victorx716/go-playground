b08ccbd5af81623c2df3a622f22cb7b5d85ff9d4
Victor Liu <victorliu716@gmail.com>
2020-02-11 16:09:41 -0500

    Begin creating go channel to check site status

---
+++ channels/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	links := []string {
+		"http://google.com",
+		"http://coinmarketcap.com",
+		"http://facebook.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+		
+	}
+}
