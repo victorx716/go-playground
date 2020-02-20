82c4edfa5fc19dc89bc6873646b66a95085e27d5
Victor Liu <vliu@ibm.com>
2020-02-20 16:57:35 -0600

    Refactor channels with function literal

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,8 +21,11 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
@@ -29,9 +33,9 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "may be down")
-		c <- "Might be down, possibly"
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
-	c <- "It is actually up"
+	c <- link
 }
