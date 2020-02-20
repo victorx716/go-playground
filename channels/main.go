14752a2b84d9c892f13fb6ab8b65e62dd7119890
Victor Liu <vliu@ibm.com>
2020-02-20 14:41:16 -0600

    Add multiple channels to have non-blocking http calls

--- channels/main.go
+++ channels/main.go
@@ -13,17 +13,27 @@ func main() {
 		"http://golang.org",
 		"http://amazon.com",
 	}
+	// free to pass around channel c
+	c := make(chan string)
 
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "may be down")
+		c <- "Might be down, possibly"
 		return
 	}
 	fmt.Println(link, "is up!")
+	c <- "It is actually up"
 }
